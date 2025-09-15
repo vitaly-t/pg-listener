ac5652b28d1c98faaac8f03fdceb695d14ce7cef
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2025-09-15 17:52:17 +0100

    create sql added

---
+++ test/sql/create.sql
@@ -0,0 +1,51 @@
+CREATE TABLE IF NOT EXISTS table_1
+(
+    id  SERIAL PRIMARY KEY,
+    msg TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS table_2
+(
+    id  SERIAL PRIMARY KEY,
+    msg TEXT NOT NULL
+);
+
+CREATE OR REPLACE FUNCTION trigger_func_1()
+    RETURNS TRIGGER
+    LANGUAGE PLPGSQL
+AS
+$$
+DECLARE
+    v_txt text;
+BEGIN
+    v_txt := format('%s', NEW);
+    PERFORM pg_notify('channel_1', v_txt);
+    RETURN NEW;
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION trigger_func_2()
+    RETURNS TRIGGER
+    LANGUAGE PLPGSQL
+AS
+$$
+DECLARE
+    v_txt text;
+BEGIN
+    v_txt := format('%s', NEW);
+    PERFORM pg_notify('channel_2', v_txt);
+    RETURN NEW;
+END;
+$$;
+
+CREATE OR REPLACE TRIGGER insert_trigger_1
+    AFTER INSERT
+    ON table_1
+    FOR EACH ROW
+EXECUTE FUNCTION trigger_func_1();
+
+CREATE OR REPLACE TRIGGER insert_trigger_2
+    AFTER INSERT
+    ON table_2
+    FOR EACH ROW
+EXECUTE FUNCTION trigger_func_2();
