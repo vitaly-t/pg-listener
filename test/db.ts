727e87eb3ca17689343e33882c785020cf4a6727
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2025-09-15 18:15:21 +0100

    adding db support

---
+++ test/db.ts
@@ -0,0 +1,13 @@
+import pgPromise from 'pg-promise';
+
+export const pgp = pgPromise({
+    capSQL: true
+});
+
+export const db = pgp({
+    user: process.env.PG_USER || 'postgres',
+    password: process.env.PG_PASSWORD || '',
+    database: process.env.PG_DATABASE || 'postgres',
+    port: process.env.PG_PORT ? parseInt(process.env.PG_PORT) : 5432,
+    allowExitOnIdle: true
+});
